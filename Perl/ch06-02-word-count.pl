73a85368737169c4d61847a16cd04ee5a831b0ec
Ramkumar Krishnan <ramkrish@localhost.localdomain>
2020-11-05 21:16:11 -0800

    Ch.06 hash examples

---
+++ Perl/ch06-02-word-count.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+#Print name given the index
+
+my $sznames = 10;
+print "Hash table size: $sznames\n";
+
+my %hashname;
+my $idx = 0;
+print "Enter name (^D to exit): ";
+while ($name = <STDIN>)
+{
+    chomp($name);
+    if (!$hashname{$name})
+    {
+	$hashname{$name} = 1;  #initialize
+    }
+    else
+    {
+	@hashname{$name} += 1; # increment
+    };
+    # $hashname{$name} = $name;
+    last if ($idx++ == $sznames);
+
+    print "Enter name (^D to exit): ";
+}
+
+print "\n";
+while (($key, $value) = each %hashname)
+{
+    print "$key => $value\n";
+}
+
+print "Enter name (^D to exit): ";
+while ($name = <STDIN>)
+{
+    chomp($name);
+
+    print "Count: $hashname{$name}\n";
+
+    print "Enter name (^D to exit): ";
+}
